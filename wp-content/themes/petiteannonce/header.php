06a5465741186a591c050ff69613639c44365238
armandfardeau <fardeauarmand@gmail.com>
2016-10-25 15:44:03 +0200

    [FRONT][CSS][ASSET] Fix CSS issue

--- wp-content/themes/petiteannonce/header.php
+++ wp-content/themes/petiteannonce/header.php
@@ -25,7 +25,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="<?php echo home_url(); ?>">
+                        <a class="navbar-brand brand" href="<?php echo home_url(); ?>">
                             <?php bloginfo('name'); ?>
                         </a>
                     </div>
