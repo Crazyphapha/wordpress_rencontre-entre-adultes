a3a043d37c6bbaa7368997977badaadd6ec6ed6f
armandfardeau <fardeauarmand@gmail.com>
2016-10-26 16:37:24 +0200

    [FRONT][CSS] clean footer design

--- wp-content/themes/petiteannonce/footer.php
+++ wp-content/themes/petiteannonce/footer.php
@@ -1,10 +1,22 @@
-        <footer class ="footer" id="footer">
-            <div class="footer-container container-fluid">
-                <nav class="navbar">
-                    <?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
-                </nav>
-            </div>
-        </footer>
+<div class="footer">
+    <div class="footer-widgets-container">
+        <div class="footer-widget">
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-1') ) : ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="footer-widget">
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-2') ) : ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="footer-widget">
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-3') ) : ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+
 
         <!-- Execution de la fonction wp_footer() obligatoire ! -->
         <?php wp_footer();  ?>
