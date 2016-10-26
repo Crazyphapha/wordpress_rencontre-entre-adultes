01fa8e581aabfd0ea971299db9455196e7deb3a3
armandfardeau <fardeauarmand@gmail.com>
2016-10-26 10:52:29 +0200

    [CSS][responsive] add breakpoint to article img to resize width

--- wp-content/themes/petiteannonce/front-page.php
+++ wp-content/themes/petiteannonce/front-page.php
@@ -29,56 +29,54 @@
                 $the_query->the_post();
                 ?>
                 <section>
-                <article class="col-md-4" id="annonce-<?php the_ID();?>">
-                    <div class="profil">
-                        <div class="responsiveImg">
-                            <?php
-                            if (has_post_thumbnail()){
-                                the_post_thumbnail("thumbnail_annonce_small");
-                            }
-                            ?>
-                        </div>
-                        <h3><?php the_title();?></h3>
-                        <div class="content">
-                            <div>
-                                <?php the_content();?>
-                            </div>
-                            <a href="<?php the_permalink();?>">La suite...</a>
-                        </div>
-                        <div class="profil-type">
-                            <div class="genre">
-                                <strong>Genre: </strong>
-                                <?php the_field("genre"); ?>
-                            <?php
-
-                                if (get_field("genre")== 'Homme'){
-                                    echo ('<span class="glyphicons glyphicons-gender-male"></span>');
+                    <article class="col-md-4" id="annonce-<?php the_ID();?>">
+                        <div class="profil">
+                            <div class="responsiveImg">
+                                <?php
+                                if (has_post_thumbnail()){
+                                    the_post_thumbnail("thumbnail_annonce_small");
                                 }
-                                elseif(get_field("genre")== 'Femme'){
-                                    echo ('<span class="glyphicons glyphicons-gender-female"></span>');
-                                }
-                                else{
-                                    echo ('<span class="glyphicons glyphicons-gender-intersex"></span>');
-                                }
+                                ?>
+                            </div>
+                            <h3><?php the_title();?></h3>
+                            <div class="content">
+                                <div>
+                                    <?php the_content();?>
+                                </div>
+                                <a href="<?php the_permalink();?>">La suite...</a>
+                            </div>
+                            <div class="profil-type">
+                                <div class="genre">
+                                    <strong>Genre: </strong>
+                                    <?php the_field("genre"); ?>
+                                <?php
 
-                            ?>
+                                    if (get_field("genre")== 'Homme'){
+                                        echo ('<span class="glyphicons glyphicons-gender-male"></span>');
+                                    }
+                                    elseif(get_field("genre")== 'Femme'){
+                                        echo ('<span class="glyphicons glyphicons-gender-female"></span>');
+                                    }
+                                    else{
+                                        echo ('<span class="glyphicons glyphicons-gender-intersex"></span>');
+                                    }
+
+                                ?>
+                                </div>
+                                <div class="Passion">
+                                    <strong>Passion: </strong>
+                                    <?php the_field("passion"); ?>
+                                </div>
+                                <div class="ville">
+                                    <strong>Ville: </strong>
+                                    <?php the_field("ville"); ?>
+                                </div>
                             </div>
-                            <div class="Passion">
-                                <strong>Passion: </strong>
-                                <?php the_field("passion"); ?>
-                            </div>
-                            <div class="ville">
-                                <strong>Ville: </strong>
-                                <?php the_field("ville"); ?>
+                            <div class="btn-profil-container">
+                                <a href="<?php the_permalink();?>" class="btn btn-info btn-profil" role="button">Voir son profil</a>
                             </div>
                         </div>
-                        <div class="btn-profil-container">
-                            <a href="<?php the_permalink();?>" class="btn btn-info btn-profil" role="button">Voir son profil</a>
-                        </div>
-                    </div>
-
-
-                </article>
+                    </article>
                 </section>
                 <?php
             }
