7c5f85f19fb6a68c9399d55e6348991c764dad5c
armandfardeau <fardeauarmand@gmail.com>
2016-10-26 16:48:19 +0200

    [FRONT][PAGE] clean PHP

--- wp-content/themes/petiteannonce/Template_qui_sommes_nous.php
+++ wp-content/themes/petiteannonce/Template_qui_sommes_nous.php
@@ -6,19 +6,7 @@ Template Name: Qui sommes nous
 ?>
 
 <?php get_header(); //appel du template header.php ?>
-<?php
-$args=array('page_id' => '46');
-// The Query
-$the_query = new WP_Query( $args );
 
-// The Loop
-if ($the_query->have_posts()) {
-    while ($the_query->have_posts()) {
-        $the_query->the_post();
-    }
-}
-?>
-<?php the_content();?>
 <?php the_field("statut_juridique"); ?>
 
 <?php wp_reset_postdata();?>
